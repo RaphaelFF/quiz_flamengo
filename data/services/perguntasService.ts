898e9976bb195d63a9d8fe315ec0b9f0ee6d6467
Raphael Fernandes Franca <116182607+RaphaelFF@users.noreply.github.com>
2025-03-28 18:49:45 -0300

    feat: adicionando a funcionalidade de dados com o superbase

--- data/services/perguntasService.ts
+++ data/services/perguntasService.ts
@@ -1,13 +1,24 @@
-import axios from 'axios'
+import { createClient } from '@supabase/supabase-js';
+
+// Configuração do Supabase
+const supabaseUrl = 'https://blxwoqiaarfjnxodbftr.supabase.co'; // Substitua pela URL do seu projeto
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJseHdvcWlhYXJmam54b2RiZnRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxOTQ5ODcsImV4cCI6MjA1ODc3MDk4N30.NEiyQZkVK7z1E8bjFTzDdSjwCQS2tP9FNfsWzJClypU'; // Substitua pela chave pública do Supabase
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 export const obterPerguntas = async () => {
-    const response = await axios.get('https://raphaelff.github.io/quiz-flamengo-data/perguntas.json')
-    const data = response.data
+    try {
+        const { data, error } = await supabase
+            .from('perguntas')
+            .select('*');
 
-    // Verifica se o dado retornado é um array
-    if (!Array.isArray(data)) {
-        throw new Error('O formato do JSON não é válido. Esperado um array de perguntas.')
+        if (error) {
+            console.error('Erro ao obter perguntas:', error);
+            throw new Error('Erro ao acessar o Supabase.');
+        }
+
+        return data;
+    } catch (error) {
+        console.error('Erro ao obter perguntas:', error);
+        throw new Error('Erro ao acessar o backend.');
     }
-
-    return data
-}
+};
